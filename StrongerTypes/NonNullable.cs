857d7480f5afbce148555afccf46d92275681821
Matt Kotsenas <matt.kotsenas@gmail.com>
2013-07-15 19:03:01 -0700

    Add NonNullable type and unit tests
    Added basic NonNullable type and associated unit tests.

---
+++ StrongerTypes/NonNullable.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StrongerTypes
+{
+    /// <summary>
+    /// A small wrapper around reference types that specifies that the reference will not be null.
+    /// Think of this type as the opposite of Nullable&lt;T&gt; (e.g. int?). The goal is to provide callers
+    /// with the additional context that null is never an expected value and should be considered an error.
+    ///
+    /// It is possible to create a null reference by using the default, parameterless constructor that
+    /// comes with every struct, though this is difficult to do in practice as using the parameterless
+    /// constructor in most cases requires setting the fields prior to use and the <see cref="Value"/> field
+    /// is read only. In these cases the wrapper will throw an exception on access to the value.
+    /// </summary>
+    /// <remarks>
+    /// Original inspiration comes from http://msmvps.com/blogs/jon_skeet/archive/2008/10/06/non-nullable-reference-types.aspx.
+    /// </remarks>
+    /// <typeparam name="T">The type to wrap.</typeparam>
+    public struct NonNullable<T> where T : class
+    {
+        /// <summary>
+        /// The actual object / value we are holding onto.
+        /// </summary>
+        private readonly T value;
+
+        /// <summary>
+        /// Create a new wrapper around an instance of <paramref name="value"/> of type <typeparamref name="T"/>.
+        /// </summary>
+        /// <param name="value">The not null object to wrap.</param>
+        public NonNullable(T value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException("value");
+            }
+            this.value = value;
+        }
+
+        /// <summary>
+        /// The unwrapped object. Throws an <see cref="InvalidOperationException"/> if the value
+        /// is null (e.g. by using the parameterless constructor to create the wrapper).
+        /// </summary>
+        public T Value
+        {
+            get
+            {
+                if (value == null)
+                {
+                    throw new InvalidOperationException("Value is null");
+                }
+                return value;
+            }
+        }
+    }
+}
---
+++ UnitTests/NonNullableTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StrongerTypes;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class NonNullableTests
+    {
+        private class SampleClass
+        {
+            int SampleField
+            {
+                get { return 7; }
+            }
+        }
+
+        [TestMethod]
+        public void InstantiationTest()
+        {
+            var sample = new SampleClass();
+            var test = new NonNullable<SampleClass>(sample);
+
+            Assert.AreEqual(sample, test.Value);
+        }
+
+        [TestMethod]
+        public void NullInstantiationTest()
+        {
+            bool exceptionThrown = false;
+
+            try
+            {
+                var test = new NonNullable<SampleClass>(null);
+            }
+            catch (ArgumentNullException)
+            {
+                exceptionThrown = true;
+            }
+
+            Assert.IsTrue(exceptionThrown, "ArgumentNullException is expected when instantiating with a null instance");
+        }
+
+        [TestMethod]
+        public void ParameterlessConstructorTest()
+        {
+            bool exceptionThrown = false;
+            var test = new NonNullable<SampleClass>();
+
+            try
+            {
+                var temp = test.Value;
+            }
+            catch (InvalidOperationException)
+            {
+                exceptionThrown = true;
+            }
+
+            Assert.IsTrue(exceptionThrown, "InvalidOperationException is expected when creating a NonNullable via the parameterless contrustor");
+        }
+    }
+}
